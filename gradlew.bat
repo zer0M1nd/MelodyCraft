bab99ee506c8177d1f8b3069642399dd6af50a92
zer0M1nd <36225378+zer0M1nd@users.noreply.github.com>
2023-01-20 12:16:19 +0800

    update

--- gradlew.bat
+++ gradlew.bat
@@ -16,8 +16,34 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-set JAVA_HOME=D:/Program Files/Java/jdk1.8.0_311
-set JAVA_EXE=D:/Program Files/Java/jdk1.8.0_311/jre/bin/java.exe
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
 
 :init
 @rem Get command-line arguments, handling Windowz variants
